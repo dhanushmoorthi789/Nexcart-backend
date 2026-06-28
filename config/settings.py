041eef3a9d27a2326890d07b916f182606df3576
Your Name <dhanushmoorthi789@gmail.com>
2026-06-28 13:31:37 +0530

    commit

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as static_serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/products/', include('products.urls')),
     path('api/orders/', include('orders.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# static() only registers routes when DEBUG=True, so on Render (DEBUG=False)
+# the /media/ and /static/ URLs were never being added -> 404 on every image.
+# We add explicit fallback routes so media/static files are served even in
+# production. WhiteNoise already serves /static/, but /media/ needs this.
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', static_serve, {'document_root': settings.MEDIA_ROOT}),
+]
